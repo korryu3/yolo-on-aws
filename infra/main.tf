a16f9e80046df182c2e02b6badae6b1699f260be
KorRyu3 <146335193+KorRyu3@users.noreply.github.com>
2025-10-16 18:47:57 +0900

    terraform state managed by s3 backend

--- infra/main.tf
+++ infra/main.tf
@@ -10,3 +10,14 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "yolo-on-aws-terraform-state"
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
